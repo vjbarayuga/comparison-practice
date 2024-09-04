b103a2f7b241d9e11872a841ad2583b1e7e4af44
vjdbarayuga <vjdbarayuga@gmail.com>
2024-09-04 23:37:14 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let x = 10;
+let y = 11;
+
+//Use comparison operators and store the results in variables
+let isXGreaterThanY = x > y;
+let isXLessThanY = x < y;
+let isXEqualToY = x === y;
+let isXNotEqualToY = x !== y;
+
+console.log('Is X greater than Y?', isXGreaterThanY);
+console.log('Is X less than Y?', isXLessThanY);
+console.log('Is X equal to Y?', isXEqualToY);
+console.log('Is X not equal to Y?', isXNotEqualToY);
